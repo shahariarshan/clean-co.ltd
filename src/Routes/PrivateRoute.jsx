c2891912107fbc9a91d649b2d2f95f95648066be
unknown <shahariarmohammadhassan@gmail.com>
2023-11-03 02:09:55 +0600

    log in & register

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import useAuth from "../Hooks/useAuth";
 const PrivateRoute = ({children}) => {
     const {user,isLoading }=useAuth()
     if(isLoading){
-        return <progress className="progress my-28 mx-auto "></progress>
+        return <progress className="progress my-28 mx-auto w-[100vh]"></progress>
     }
 
 
